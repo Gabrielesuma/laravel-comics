76571af1b24c7c01b66ce8a5d96d8bff60c4a933
Gabrielesuma <gabrielesuma139@gmail.com>
2024-05-20 20:10:39 +0200

    main series

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,19 @@
 
 @section('content')
 <section class="container">
-    <h1>home</h1>
-    <p>questa è l'home page</p>
+    <div class="row">
+
+        @foreach ($series as $serie)
+            <div class="col-12 col-md-6 col-lg-2 mb-4">
+                <div class="card">
+                    <img src="{{$serie['thumb']}}" class="card-img-top" alt="Thumb">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$serie['title']}}</h5>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
 </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $series = config('comics');
+    return view('home', compact('series'));
 })->name('home');
